e35b89ccc44eef081e50da32763b5aa45992543f
youssefcaid <131032877+youssefcaid@users.noreply.github.com>
2023-04-18 11:21:59 +0000

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,246 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quel est le nombre de bit d'une adresse IPv4 ?",
+    answer: "32",
+    options: [
+      "35",
+      "23",
+      "24",
+      "32"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Routeur est un équipement de la couche ?",
+    answer: "Couche réseau",
+    options: [
+      "Couche physique",
+      "Couche réseau",
+      "Couche liaison de données",
+      "Couche application"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quel élément définit l'interface principle utilisée pour une configuration initiale d'un routeur cisco ?",
+    answer: "Une interface console",
+    options: [
+      "Une interface AUX",
+      "Une interface Ethernet ",
+      "Une interface console",
+      "Une interface Série"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quel est l'intrus",
+    answer: "PHP",
+    options: [
+      "TCP",
+      "UDP",
+      "PHP",
+      "ICMP"
+    ]
+  },
+    {
+		
+    numb: 5,
+    question: "Quel port est associé au protocole SMTP",
+    answer: "25",
+    options: [
+      "80",
+      "53",
+      "25",
+      "35"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Choisir parmi les listes suivantes celle qui constituée de protocoles des routages",
+    answer: "RIP,OSPF,EIGRP",
+    options: [
+      "RIP,OSPF,EIGRP",
+      "FTP,DNS,HTTP",
+      "TCP,UDP,IP",
+      "Ethernet,RNIS,ATM"
+    ]
+},
+{
+    numb: 7,
+    question: "Quel genre de prise est utilisé de manière standard pour les prises de télécommunications ?",
+    answer: "RJ45",
+    options: [
+  "TIA74",
+  "EIA45",
+  "RJ45",
+  "RG54"
+]
+  },
+    {
+    numb: 8,
+    question: "Le nombre binaire 1011 vaut en décimal",
+    answer: "11",
+    options: [
+  "11",
+  "8",
+  "9",
+  "12"
+]
+  },
+    {
+    numb: 9,
+    question: "Quel est le nombre de bit d'une adresse ipv6",
+    answer: "128",
+    options: [
+  "64",
+  "48",
+  "128",
+  "46",
+]
+  },
+    {
+    numb: 10,
+    question: "La trame est une unité de données de protocole (PDU) de la couche ?",
+    answer: "Liaison de Données",
+    options: [
+  "Application",
+  "Session",
+  "Liaison de Données",
+  "Physique",
+   ]
+  },
+    {
+   numb: 11,
+    question: "Quel protocole est utilisé pour un nom de domain ?",
+    answer: "DNS",
+    options: [
+      "FTP",
+      "ARP",
+      "DHCP",
+      "DNS"
+   ]
+  },
+    {
+   numb: 12,
+    question: "Lequel est un outil de supervision / monitoring réseau",
+    answer: "nagios",
+    options: [
+      "ssh",
+      "ping",
+      "telnet",
+      "nagios"
+   ]
+  },
+    {
+   numb: 13,
+    question: "Parmi ces technologies, laquelle permet de créer un VPN",
+    answer: "IPsec",
+    options: [
+      "IPv6",
+      "ICMP",
+      "IPsec",
+      "SDSL"
+   ]
+  },
+    {
+   numb: 14,
+    question: "Parmi ces propositions, laquelle n'est pas un algorithme de chiffrement ?",
+    answer: "TAR",
+    options: [
+      "AES",
+      "TAR",
+      "3DES",
+      "Blowfish",
+   ]
+  },
+    {
+   numb: 15,
+    question: "Quelle est l'adresse localhost?",
+    answer: "127.0.0.1",
+    options: [
+      "255.255.255.0",
+      "192.168.0.1",
+      "127.0.0.1",
+      "172.0.0.1",
+
+ ]
+  },
+    {
+    numb: 16,
+    question: "Quel équipement réseau prend des décisions de transfert en fonction de l'adresse MAC de destination dans la trame?",
+    answer: "Commutateur",
+    options: [
+  "Répéteur",
+  "Commutateur",
+  "Routeur",
+  "Concentrateur",
+  ]
+  },
+    {
+    numb: 17,
+    question: "Parmi les adresses IP suivantes, laquelle n'est pas valide ?",
+    answer: "192.168.276.1",
+    options: [
+  "172.16.1.254",
+  "1.2.3.4",
+  "147.126.0.9",
+  "192.168.276.1",
+  ]
+  },
+    {
+    numb: 18,
+    question: "Quel algorithme est de type asymétrique?",
+    answer: "RSA",
+    options: [
+  "DES",
+  "RSA",
+  "3DES",
+  "AES",
+  ]
+  },
+    {
+    numb: 19,
+    question: "Quel est le port utilisé par défaut par le service NTP?",
+    answer: "123",
+    options: [
+  "123",
+  "443",
+  "110",
+  "8080",
+  ]
+  },
+    {
+    numb: 20,
+    question: "WI-FI signifie",
+	answer: "Wireless-Fidelity",
+    options: [
+  "Wireless-Fidelity",
+  "Windows Fi",
+  "Windows Fidelity",
+  "Wireless-Fi",
+  ]
+  },
+];
+
+
+
+
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //     ]
+  //    },
+  //    ];
